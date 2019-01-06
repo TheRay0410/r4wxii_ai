4440d955643c7a259e3b6bef2f33ce9b07f65857
TheRay0410 <yn041010@gmail.com>
2019-01-07 00:00:04 +0900

    add:user auth

--- main.kt
+++ main.kt
@@ -1,10 +1,18 @@
-import jp.nephy.penicillin.*
+package com.r4wxii.r4wxii_ai
+
+import jp.nephy.penicillin.PenicillinClient
+import jp.nephy.penicillin.core.emulation.EmulationMode
+import java.util.concurrent.TimeUnit
 
 fun main() {
   val client = PenicillinClient {
-      account {
-          application("ConsumerKey", "ConsumerSecret")
-          token("AccessToken", "AccessTokenSecret")
-      }
+    account {
+        application(System.getenv("consumerKey"), System.getenv("consumerSecret"))
+        token(System.getenv("accessToken"), System.getenv("accessTokenSecret"))
+    } 
+    emulationMode = EmulationMode.TwitterForiPhone  // Twitter for iPhone 準拠のリクエストを送る
+
+    maxRetries = 5  // API エラー発生時のリトライを最大で5回まで行う
+    retry(1, TimeUnit.SECONDS)  // API エラー発生時のリトライに1秒間隔をあける
   }
 }
