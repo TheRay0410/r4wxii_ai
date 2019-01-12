ed2f647560815c06e70bb86a65bd926ac24ca2f3
TheRay0410 <yn041010@gmail.com>
2019-01-12 21:21:05 +0900

    remove:client

--- src/main/kotlin/com/r4wxii/R4wxiiAI.kt
+++ src/main/kotlin/com/r4wxii/R4wxiiAI.kt
@@ -1,21 +1,8 @@
 package com.r4wxii
 
-import jp.nephy.penicillin.PenicillinClient
 import kotlinx.coroutines.*
 
 fun main() {
-    val key = KeyData()
-    val client = PenicillinClient {
-        account {
-            application(key.ConsumerKey, key.ConsumerSecret)
-            token(key.AccessToken, key.AccessTokenSecret)
-        }
-    }
-    runBlocking  {
-        client.timeline.user(screenName = "TheRay_Misc", count = 100).await().forEach { status ->
-            // prints status text.
-            println(status.text)
-        }
-    }
-    client.close()
+    val tweet = Tweet()
+    tweet.post()
 }
