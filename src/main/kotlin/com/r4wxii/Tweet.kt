7d17d88ee46344c3850143bcb7c5fd2046943d79
TheRay0410 <yn041010@gmail.com>
2019-01-12 21:23:07 +0900

    add:TweetClass

---
+++ src/main/kotlin/com/r4wxii/Tweet.kt
@@ -0,0 +1,34 @@
+package com.r4wxii
+
+import jp.nephy.penicillin.PenicillinClient
+import jp.nephy.penicillin.core.emulation.EmulationMode
+import jp.nephy.penicillin.endpoints.Statuses
+import jp.nephy.penicillin.extensions.complete
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
+
+
+class Tweet {
+    val key = KeyData()
+    val client = PenicillinClient {
+        account {
+            application(key.ConsumerKey, key.ConsumerSecret)
+            token(key.AccessToken, key.AccessTokenSecret)
+        }
+        emulationMode = EmulationMode.TwitterForiPhone
+
+        maxRetries = 5
+        retry(1, TimeUnit.SECONDS)
+    }
+    fun getTweet(user: String) {
+        runBlocking  {
+            client.timeline.user(screenName = user, count = 100).await().forEach { status ->
+                // prints status text.
+
+            }
+        }
+    }
+    fun post(text: String = "テスト") {
+        client.statuses.update(status = text).complete()
+    }
+}
