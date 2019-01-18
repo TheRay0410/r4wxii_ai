05295f5909baa8ab8c74254dc82fe7d9708e3b75
TheRay0410 <yn041010@gmail.com>
2019-01-18 22:50:13 +0900

    fix:START & END identifier

--- src/main/kotlin/com/r4wxii/MarkovChain.kt
+++ src/main/kotlin/com/r4wxii/MarkovChain.kt
@@ -2,12 +2,12 @@ package com.r4wxii
 
 class MarkovChain {
     fun makeBlock(blockList: MutableList<Triple<String, String, String>>, morphene: List<String>) {
-        blockList.add(Triple("START", morphene[0], morphene[1]))
+        blockList.add(Triple("_START_", morphene[0], morphene[1]))
         for(index in morphene.indices) {
             if(index < morphene.lastIndex - 1) {
                 blockList.add(Triple(morphene[index], morphene[index + 1], morphene[index + 2]))
             } else {
-                blockList.add(Triple(morphene[index], morphene[index + 1], "END"))
+                blockList.add(Triple(morphene[index], morphene[index + 1], "_END_"))
                 break
             }
         }
