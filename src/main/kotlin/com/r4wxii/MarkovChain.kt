b0d78c4375bde2bdb56eb99e4421a749e8fb8c29
TheRay0410 <yn041010@gmail.com>
2019-01-18 22:40:50 +0900

    add:MarkovChain.kt

---
+++ src/main/kotlin/com/r4wxii/MarkovChain.kt
@@ -0,0 +1,15 @@
+package com.r4wxii
+
+class MarkovChain {
+    fun makeBlock(blockList: MutableList<Triple<String, String, String>>, morphene: List<String>) {
+        blockList.add(Triple("START", morphene[0], morphene[1]))
+        for(index in morphene.indices) {
+            if(index < morphene.lastIndex - 1) {
+                blockList.add(Triple(morphene[index], morphene[index + 1], morphene[index + 2]))
+            } else {
+                blockList.add(Triple(morphene[index], morphene[index + 1], "END"))
+                break
+            }
+        }
+    }
+}
