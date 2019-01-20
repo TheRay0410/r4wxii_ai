fae948d1aff91957558c5f1fe2392882ea870ef5
TheRay0410 <yn041010@gmail.com>
2019-01-20 21:56:02 +0900

    remove:makeBlock()

--- src/main/kotlin/com/r4wxii/MarkovChain.kt
+++ src/main/kotlin/com/r4wxii/MarkovChain.kt
@@ -22,15 +22,4 @@ class MarkovChain {
         val block = blockList.filter { it.first == firstWord }[Random.nextInt(blockList.filter { it.first == firstWord }.indices)]
         return block
     }
-    fun makeBlock(blockList: MutableList<Triple<String, String, String>>, morphene: List<String>) {
-        blockList.add(Triple("_START_", morphene[0], morphene[1]))
-        for(index in morphene.indices) {
-            if(index < morphene.lastIndex - 1) {
-                blockList.add(Triple(morphene[index], morphene[index + 1], morphene[index + 2]))
-            } else {
-                blockList.add(Triple(morphene[index], morphene[index + 1], "_END_"))
-                break
-            }
-        }
-    }
 }
