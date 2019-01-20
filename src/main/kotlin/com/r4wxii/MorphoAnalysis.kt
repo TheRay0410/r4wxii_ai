f9b9a6bdaf50149f7533cb172aabb8047696f631
TheRay0410 <yn041010@gmail.com>
2019-01-20 22:00:56 +0900

    add:MorphoAnalysis.kt

---
+++ src/main/kotlin/com/r4wxii/MorphoAnalysis.kt
@@ -0,0 +1,38 @@
+package com.r4wxii
+
+import org.chasen.mecab.Tagger
+import org.chasen.mecab.Node
+
+class MorphoAnalysis {
+    val tagger = Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+    var blockList = mutableListOf<Triple<String, String, String>>()
+    fun Node.isNull(): Boolean = this.next == null
+    fun text2morphene(text: String): List<String> {
+        tagger .parse(text)
+        var node = tagger.parseToNode(text).next
+        var morphene: MutableList<String> = mutableListOf()
+        while(!node.isNull()) {
+            morphene.add(node.surface)
+            node = node.next
+        }
+        return morphene
+    }
+    fun List<String>.isOnly(): Boolean = this.count() == 1
+    fun makeBlock(blockList: MutableList<Triple<String, String, String>>, morphene: List<String>) {
+        blockList.add(if(morphene.isOnly()) {
+            Triple("_START_", morphene[0], "_END_")
+        } else {
+            Triple("_START_", morphene[0], morphene[1])
+        })
+        if(morphene.isOnly()) {
+            for (index in morphene.indices) {
+                if (index < morphene.lastIndex - 1) {
+                    blockList.add(Triple(morphene[index], morphene[index + 1], morphene[index + 2]))
+                } else {
+                    blockList.add(Triple(morphene[index], morphene[index + 1], "_END_"))
+                    break
+                }
+            }
+        }
+    }
+}
