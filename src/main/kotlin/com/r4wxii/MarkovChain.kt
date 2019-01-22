0250899add8aed349fcf81411f31b6f561e90b68
TheRay0410 <yn041010@gmail.com>
2019-01-22 17:28:57 +0900

    fix:findStartBlock()

--- src/main/kotlin/com/r4wxii/MarkovChain.kt
+++ src/main/kotlin/com/r4wxii/MarkovChain.kt
@@ -14,8 +14,7 @@ class MarkovChain {
     }
     fun findStartBlock(blockList: List<Triple<String, String, String>>): Pair<String, Triple<String, String, String>> {
         val startBlock = blockList.filter { it.first == "_START_" }[Random.nextInt(blockList.filter { it.first == "_START_" }.indices)]
-        val initText: String = startBlock.second + startBlock.third
-        return Pair(initText, startBlock)
+        return Pair(startBlock.second, startBlock)
     }
     fun findBlock(blockList: List<Triple<String, String, String>>,firstWord: String): Triple<String, String, String> {
         val block = blockList.filter { it.first == firstWord }[Random.nextInt(blockList.filter { it.first == firstWord }.indices)]
