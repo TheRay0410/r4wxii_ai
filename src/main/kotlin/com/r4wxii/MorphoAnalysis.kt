55f1c1361b785a6ae36e8393c1ef8e6bd47ead27
TheRay0410 <yn041010@gmail.com>
2019-01-23 21:49:22 +0900

    change:extensionFun to scopeFun

--- src/main/kotlin/com/r4wxii/MorphoAnalysis.kt
+++ src/main/kotlin/com/r4wxii/MorphoAnalysis.kt
@@ -9,25 +9,23 @@ class MorphoAnalysis {
     }
     val tagger = Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
     var blockList = mutableListOf<Triple<String, String, String>>()
-    fun Node.isNull(): Boolean = this.next == null
     fun text2morphene(text: String): List<String> {
         tagger.parse(text)
         var node = tagger.parseToNode(text).next
         var morphene: MutableList<String> = mutableListOf()
-        while(!node.isNull()) {
+        while(node.run { next != null}) {
             morphene.add(node.surface)
             node = node.next
         }
         return morphene
     }
-    fun List<String>.isOnly(): Boolean = this.count() == 1
     fun makeBlock(blockList: MutableList<Triple<String, String, String>>, morphene: List<String>) {
-        blockList.add(if(morphene.isOnly()) {
+        blockList.add(if(morphene.run { count() == 1}) {
             Triple("_START_", morphene[0], "_END_")
         } else {
             Triple("_START_", morphene[0], morphene[1])
         })
-        if(!morphene.isOnly()) {
+        if(morphene.run { count() != 1}) {
             for (index in morphene.indices) {
                 if (index < morphene.lastIndex - 1) {
                     blockList.add(Triple(morphene[index], morphene[index + 1], morphene[index + 2]))
