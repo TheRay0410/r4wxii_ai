b2878acf35e703e7620d876ea2f2b48d8c213d1a
TheRay0410 <yn041010@gmail.com>
2019-11-30 14:15:06 +0900

    update:なんかいい感じに

--- src/main/kotlin/com/r4wxii/MorphoAnalysis.kt
+++ src/main/kotlin/com/r4wxii/MorphoAnalysis.kt
@@ -10,11 +10,11 @@ class MorphoAnalysis {
     val tagger = Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
     var blockList = mutableListOf<Triple<String, String, String>>()
     fun text2morpheme(text: String): List<String> {
-        if(text == "") return emptyList()
+        if(text.isBlank()) return emptyList()
         tagger.parse(text)
         var node = tagger.parseToNode(text).next
-        var morpheme: MutableList<String> = mutableListOf()
-        while(node.run { next != null }) {
+        val morpheme: MutableList<String> = mutableListOf()
+        while(node.next != null) {
             morpheme.add(node.surface)
             node = node.next
         }
@@ -26,7 +26,7 @@ class MorphoAnalysis {
             1 -> blockList.add(Triple("_START_", morpheme[0], "_END_"))
             else -> blockList.add(Triple("_START_", morpheme[0], morpheme[1]))
         }
-        if(morpheme.run { count() > 1}) {
+        if(morpheme.count() > 1) {
             for (index in morpheme.indices) {
                 if (index < morpheme.lastIndex - 1) {
                     blockList.add(Triple(morpheme[index], morpheme[index + 1], morpheme[index + 2]))
